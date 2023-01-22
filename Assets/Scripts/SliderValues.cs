92756cb0ebd29d4d8d62451d7cfd07e34cdc7242
Monstrong <igrachev2006@gmail.com>
2023-01-22 16:57:06 +0300

    Years Added

---
+++ Assets/Scripts/SliderValues.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderValues : MonoBehaviour
+{
+    public Slider slider
+    public GameObject first
+    void Update()
+    {
+        if (slider = 1)
+        {
+            first.SetActive(true);
+        }
+        else
+        {
+            first.SetActive(false);
+        }
+    }
+}
